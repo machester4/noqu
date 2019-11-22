cadc1c2e7bc98c69aefbb33c8fed084c254fd7f0
Michael P <mpintos@lithium.com.uy>
2019-11-22 11:49:54 -0300

    Test implement noqu-board-connector

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ import TestController from "./app/controller/TestController";
 import NotificationController from "./app/controller/NotificationController";
 import Queue from "./app/lib/Queue";
 
+// Noqu-board-connector
+// draft folder is fake node_modules clone noqu-board-connector here
+// const connector = require("../draft");
+
 const app = express();
 const port = process.env.port || 3333;
 //BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
@@ -14,6 +18,9 @@ app.use(express.json());
 app.post("/test", TestController);
 // This is only for example hook notification
 app.post("/notification", NotificationController);
+
+// Use Noqu-board-connector routes
+// connector(app, "/app", null);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
